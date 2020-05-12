830c69602ed9b98402b30a899bd16288e18399df
ffettes <ffettes@cli-raw.image>
2020-05-12 18:23:51 +0000

    tryed a water gpu thing but it was too advanced

--- my_script.js
+++ my_script.js
@@ -1,18 +1,16 @@
 var THREE = require("three")
 var camera, scene, renderer;
 var geometry, material, mesh;
+if (module.hot) { module.hot.accept() }
+init(); animate();
 
-init();
-animate();
 
 function init() {
-
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
     camera.position.z = 1;
-
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    geometry = new THREE.BoxGeometry( 0.2, 0.8, 0.2 );
     material = new THREE.MeshNormalMaterial();
 
     mesh = new THREE.Mesh( geometry, material );
@@ -21,16 +19,11 @@ function init() {
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-
 }
 
 function animate() {
-
     requestAnimationFrame( animate );
-
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
-
     renderer.render( scene, camera );
-
 }
