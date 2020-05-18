c7993d4b0a14e51e21ddcba6abaadf6d46634a13
fergus <fergusfettes@gmail.com>
2020-05-18 22:49:37 +0200

    finished with the solar system, playing with the tank example now

--- www/scripts/background.js
+++ www/scripts/background.js
@@ -1,36 +1,82 @@
 import * as THREE from "three";
-export { canvas, renderer, camera, scene };
+export { canvas, renderer, camera, scene, makeCamera };
 
 let canvas, renderer, camera, scene;
 
 makeBackground();
-export function makeBackground() {
+function makeBackground() {
 
   canvas = document.querySelector('#c');
   renderer = new THREE.WebGLRenderer({canvas});
 
-  const fov = 40;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 1000;
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 120;
+  renderer.setClearColor(0xAAAAAA);
+  renderer.shadowMap.enabled = true;
+
+  camera = makeCamera();
+  camera.position.set(8, 4, 10).multiplyScalar(3);
+  camera.lookAt(0, 0, 0);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xCCCCCC);
 
   {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 20, 0);
     scene.add(light);
+    light.castShadow = true;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+
+    const d = 50;
+    light.shadow.camera.left = -d;
+    light.shadow.camera.right = d;
+    light.shadow.camera.top = d;
+    light.shadow.camera.bottom = -d;
+    light.shadow.camera.near = 1;
+    light.shadow.camera.far = 50;
+    light.shadow.bias = 0.001;
   }
   {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(1, -2, -4);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 2, 4);
     scene.add(light);
   }
+
+  const groundGeometry = new THREE.PlaneBufferGeometry(50, 50);
+  const groundMaterial = new THREE.MeshPhongMaterial({color: 0xCC8866});
+  const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+  groundMesh.rotation.x = Math.PI * -.5;
+  groundMesh.receiveShadow = true;
+  scene.add(groundMesh);
+
+
+  // standard lighting?
+  // {
+  //   const color = 0xFFFFFF;
+  //   const intensity = 1;
+  //   const light = new THREE.DirectionalLight(color, intensity);
+  //   light.position.set(-1, 2, 4);
+  //   scene.add(light);
+  // }
+  // {
+  //   const color = 0xFFFFFF;
+  //   const intensity = 1;
+  //   const light = new THREE.DirectionalLight(color, intensity);
+  //   light.position.set(1, -2, -4);
+  //   scene.add(light);
+  // }
+  // {
+  //   const color = 0xFFFFFF;
+  //   const intensity = 3;
+  //   const light = new THREE.PointLight(color, intensity);
+  //   scene.add(light);
+  // }
+
+}
+
+function makeCamera(fov = 40) {
+  const aspect = 2;  // the canvas default
+  const zNear = 0.1;
+  const zFar = 1000;
+  return new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
 }
---
+++ www/solar_system.js
@@ -0,0 +1,140 @@
+import * as THREE from "three";
+import {GUI} from "./src/js/third_party/dat-gui.js";
+import * as rnd from "./scripts/render.js";
+import * as mat from "./scripts/material.js";
+import * as hlp from "./scripts/helpers.js";
+import { canvas, renderer, camera, scene } from "./scripts/background.js";
+
+init();
+function init() {
+
+  const gui = new GUI();
+
+  const objects = [];
+  const slow_objects = [];
+  const points_collection = [];
+  const spread = 15;
+
+  const solarSystem = new THREE.Object3D();
+  scene.add(solarSystem);
+  slow_objects.push(solarSystem);
+
+  const earthOrbit = new THREE.Object3D();
+  earthOrbit.position.x = 15;
+  solarSystem.add(earthOrbit);
+  slow_objects.push(earthOrbit);
+
+  const moonOrbit = new THREE.Object3D();
+  moonOrbit.position.x = 2;
+  earthOrbit.add(moonOrbit);
+  slow_objects.push(moonOrbit);
+
+  // use just one sphere for everything
+  const radius = 1;
+  const widthSegments = 6;
+  const heightSegments = 6;
+  const sphereGeometry = new THREE.SphereBufferGeometry(
+      radius, widthSegments, heightSegments);
+
+  const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+  sunMesh.scale.set(5, 5, 5);  // make the sun large
+  solarSystem.add(sunMesh);
+  slow_objects.push(sunMesh);
+
+  const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
+  const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+  earthMesh.position.x = 10;
+  earthOrbit.add(earthMesh);
+  slow_objects.push(earthMesh);
+
+  const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+  moonMesh.scale.set(.5, .5, .5);
+  moonOrbit.add(moonMesh);
+  slow_objects.push(moonMesh);
+
+  function makeAxisGrid(node, label, units) {
+    const helper = new hlp.AxisGridHelper(node, units);
+    gui.add(helper, 'visible').name(label);
+  }
+
+  makeAxisGrid(solarSystem, 'solarSystem', 25);
+  makeAxisGrid(sunMesh, 'sunMesh');
+  makeAxisGrid(earthOrbit, 'earthOrbit');
+  makeAxisGrid(earthMesh, 'earthMesh');
+  makeAxisGrid(moonMesh, 'moonMesh');
+
+  function addSolidGeometry(x, y, geometry, collection) {
+    const mesh = new THREE.Mesh(geometry, mat.createMaterial());
+    addObject(x, y, mesh, collection);
+  }
+
+  function addLineGeometry(x, y, geometry, collection) {
+    const material = new THREE.LineBasicMaterial({color: 0x000000});
+    const mesh = new THREE.LineSegments(geometry, material);
+    addObject(x, y, mesh, collection);
+  }
+
+  function addObject(x, y, obj, collection) {
+    obj.position.x = x * spread;
+    obj.position.y = y * spread;
+
+    scene.add(obj);
+    collection.push(obj);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function render(time) {
+    time *= 0.001;
+
+    if (rnd.resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    objects.forEach((obj, ndx) => {
+      const speed = .1 + ndx * .0005;
+      const rot = Math.random() * speed;
+      obj.rotation.x = rot;
+      obj.rotation.y = rot;
+    });
+
+    slow_objects.forEach((obj, ndx) => {
+      const speed = .1 + ndx * .1;
+      const rot = time * speed;
+      obj.rotation.x = rot;
+      obj.rotation.y = rot;
+    });
+
+    points_collection.forEach((obj, ndx) => {
+      const speed = .1 + ndx * .01;
+      const rot = time * speed;
+      obj.rotation.x = rot;
+      obj.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
