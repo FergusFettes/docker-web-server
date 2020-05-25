7d0c070e325cce33ddefcff4360dbf361375ece3
fergus <fergusfettes@gmail.com>
2020-05-25 23:57:45 +0200

    added orbit controls

--- www-dev/3js/src/background.js
+++ www-dev/3js/src/background.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OrbitControls} from 'src/js/OrbitControls.js';
 export { canvas, renderer, camera, scene };
 
 let canvas, renderer, camera, scene;
@@ -10,8 +11,12 @@ function makeBackground() {
   renderer = new THREE.WebGLRenderer({canvas});
 
   camera = makeCamera();
-  camera.position.set(8, 4, 10).multiplyScalar(3);
+  camera.position.set(1, 2, 20).multiplyScalar(3);
   camera.lookAt(0, 0, 0);
+
+  controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
 
   renderer.setClearColor(0xAAAAAA);
   renderer.shadowMap.enabled = true;
@@ -19,19 +24,32 @@ function makeBackground() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xCCCCCC);
 
+  // {
+  //   const color = 0xFFFFFF;
+  //   const intensity = 1;
+  //   const light = new THREE.DirectionalLight(color, intensity);
+  //   light.position.set(-1, 2, 4);
+  //   scene.add(light);
+  // }
+  // {
+  //   const color = 0xFFFFFF;
+  //   const intensity = 1;
+  //   const light = new THREE.DirectionalLight(color, intensity);
+  //   light.position.set(1, -2, -4);
+  //   scene.add(light);
+  // }
+
   {
     const color = 0xFFFFFF;
     const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    const light = new THREE.AmbientLight(color, intensity);
     scene.add(light);
   }
+
   {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(1, -2, -4);
-    scene.add(light);
+    const gui = new GUI();
+    gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+    gui.add(light, 'intensity', 0, 2, 0.01);
   }
 }
 
@@ -40,4 +58,17 @@ function makeCamera(fov = 40) {
   const zNear = 0.1;
   const zFar = 1000;
   return new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
+}
+
+class ColorGUIHelper {
+  constructor(object, prop) {
+    this.object = object;
+    this.prop = prop;
+  }
+  get value() {
+    return `#${this.object[this.prop].getHexString()}`;
+  }
+  set value(hexString) {
+    this.object[this.prop].set(hexString);
+  }
 }
