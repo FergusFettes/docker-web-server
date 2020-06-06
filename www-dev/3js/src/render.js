9ec9abc6ccd4dbf3120c79959747a2be9027251e
fergus <fergusfettes@gmail.com>
2020-06-06 12:05:03 +0200

    adding new info element and making function for rotating on condition

--- www-dev/3js/src/render.js
+++ www-dev/3js/src/render.js
@@ -11,40 +11,73 @@ export {
   cubeMap,
   clearPickPosition,
 };
-let renderObjects, cubeMap, infoElem, pickHelper, pickPosition;
+let renderObjects, cubeMap, isSetActive, infoElem, infoElemBottom, pickHelper, pickPosition;
 
 renderObjects = [];
 cubeMap = new WeakMap();
+
+isSetActive = {renderObjects: true};
+
 infoElem = document.querySelector('#info');
+infoElemBottom = document.querySelector('#info-bottom');
 
 pickPosition = {x: 0, y: 0};
 pickHelper = new PickHelper();
 
 function render(time) {
   time *= 0.001;
+  conditionalPickerResizer(time);
 
+  renderObjectSet(renderObjects, time);
+  // cameraPole.rotation.y = time * .1;
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+function conditionalPickerResizer(time) {
+  if (pickHelper) {
+    pickHelper.pick(pickPosition, scene, camera, time);
+    showLink();
+  }
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
   }
+}
 
-  renderObjects.forEach((obj, ndx) => {
+function renderObjectSet(objectSet, time) {
+  if (isSetActive[objectSet]) {
+    objectSet.forEach((obj, ndx) => {
+      simpleRotate(obj, ndx, time);
+    });
+  } else {
+    objectSet.forEach((obj, ndx) => {
+      if (obj[0].type === "Mesh") {
+        simpleRotate(obj, ndx, time);
+      } else {
+        haltingRotate(obj, ndx, time);
+      }
+    });
+  }
+}
+
+function simpleRotate(obj, ndx, time) {
     const speed = .1 + ndx * .1;
     const rot = time * obj[1] * speed;
     obj[0].rotation.x = rot;
     obj[0].rotation.y = rot;
-  });
+}
 
-  // cameraPole.rotation.y = time * .1;
-
-  if (pickHelper) {
-    pickHelper.pick(pickPosition, scene, camera, time);
-    showLink();
+function haltingRotate(obj, ndx, time) {
+  const absoluteRotation = obj[0].rotation % ( Math.PI * 2 )
+  if (absoluteRotation > 0.2) {
+    const speed = .1 + ndx * .1;
+    const rot = time * obj[1] * speed;
+    obj[0].rotation.x = rot;
+    obj[0].rotation.y = rot;
   }
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
 }
 
 function resizeRendererToDisplaySize(renderer) {
@@ -83,15 +116,15 @@ function touchListeners() {
 
 function elementListeners() {
   const el1 = document.querySelector(".other-icon")
-  el1.addEventListener("click", startTransition, false)
+  el1.addEventListener("click", startTransition)
+  const el2 = document.querySelector(".third-icon")
+  el2.addEventListener("click", startTransition)
 }
 
 function startTransition(event) {
-  bringForward();
-}
-
-function bringForward () {
-  infoElem.textContent = "bringing forward my friend"
+  console.log(event)
+  infoElemBottom.textContent = "clickity click"
+  isSetActive[renderObjects] = false;
 }
 
 function clearPickPosition() {
