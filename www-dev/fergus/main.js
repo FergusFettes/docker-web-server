22e59601270b387fa367b265ed19a26eb28ea302
fergus <fergusfettes@gmail.com>
2020-06-12 22:45:33 +0200

    created elements for soundcloud and simple images

--- www-dev/fergus/main.js
+++ www-dev/fergus/main.js
@@ -8,16 +8,47 @@ init();
 requestAnimationFrame(render);
 function init() {
 
-  const video = '9xhU3sZrpiU'
-  const videos = []
-  for (let i = 0; i < 4; i ++) {
-    videos.push(video);
+  // const video = '9xhU3sZrpiU'
+  // const videos = []
+  // for (let i = 0; i < 4; i ++) {
+  //   videos.push(video);
+  // }
+
+  const logo = 'https://blog.schau-wien.at/wp-content/uploads/2020/04/logo.jpg'
+  const logos = []
+  for (let i = 0; i < 6; i ++) {
+    logos.push(logo);
+  }
+
+  const soundcloud = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/772342537&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  const soundclouds = [];
+  for (let i = 0; i < 6; i ++) {
+    soundclouds.push(soundcloud);
   }
 
   {
-    const cube = createYouCube(0, 0, 0, 480, 1, videos)
+    const cube = createYouCube(90, 0, 0, 120, 0.8, soundclouds);
     scene.add( cube );
   }
+
+  // {
+  //   const cube = createYouCube(-90, 0, 0, 120, 0.8, logos);
+  //   scene.add( cube );
+  // }
+
+  // {
+  //   const cube = createYouCube(0, 90, 0, 120, 0.8, logos);
+  //   scene.add( cube );
+  // }
+
+  // {
+  //   const cube = createYouCube(0, -90, 0, 120, 0.8, logos);
+  //   scene.add( cube );
+  // }
+
+
+
+
 
   // {
   //   const cube = createYouCube(-100, 0, 0, 480, 0, videos)
