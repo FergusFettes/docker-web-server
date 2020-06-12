92dec274543915f48037e7a6d4c3a3cb15cce379
fergus <fergusfettes@gmail.com>
2020-06-12 23:01:57 +0200

    audio working

--- www-dev/fergus/main.js
+++ www-dev/fergus/main.js
@@ -14,46 +14,43 @@ function init() {
   //   videos.push(video);
   // }
 
-  const logo = 'https://blog.schau-wien.at/wp-content/uploads/2020/04/logo.jpg'
-  const logos = []
-  for (let i = 0; i < 6; i ++) {
-    logos.push(logo);
-  }
+  // {
+  //   const cube = createYouCube(-100, 0, 0, 480, 0, videos)
+  //   scene.add( cube );
+  // }
 
-  const soundcloud = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/772342537&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-  const soundclouds = [];
-  for (let i = 0; i < 6; i ++) {
-    soundclouds.push(soundcloud);
-  }
-
-  {
-    const cube = createYouCube(90, 0, 0, 120, 0.8, soundclouds);
-    scene.add( cube );
-  }
+  // const logo = 'https://blog.schau-wien.at/wp-content/uploads/2020/04/logo.jpg'
+  // const logos = []
+  // for (let i = 0; i < 6; i ++) {
+  //   logos.push(logo);
+  // }
 
   // {
   //   const cube = createYouCube(-90, 0, 0, 120, 0.8, logos);
   //   scene.add( cube );
   // }
 
-  // {
-  //   const cube = createYouCube(0, 90, 0, 120, 0.8, logos);
-  //   scene.add( cube );
+  // const soundcloud = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/772342537&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  // const soundclouds = [];
+  // for (let i = 0; i < 6; i ++) {
+  //   soundclouds.push(soundcloud);
   // }
 
   // {
-  //   const cube = createYouCube(0, -90, 0, 120, 0.8, logos);
+  //   const cube = createYouCube(90, 0, 0, 120, 0.8, soundclouds);
   //   scene.add( cube );
   // }
 
+  const audio = 'https://storage.googleapis.com/schau-wien-images/media/jarum.mp3'
+  const audios = [];
+  for (let i = 0; i < 6; i ++) {
+    audios.push(audio);
+  }
 
-
-
-
-  // {
-  //   const cube = createYouCube(-100, 0, 0, 480, 0, videos)
-  //   scene.add( cube );
-  // }
+  {
+    const cube = createYouCube(0, 90, 0, 120, 0.8, audios);
+    scene.add( cube );
+  }
 
   // Block iframe events when dragging mainCamera
   var blocker = document.getElementById( 'blocker' );
